d920d2b929df3a442dc8168c24cce9fdf12f7293
0fk0 <greeeen_ken41ei39jin@icloud.com>
2022-06-15 17:56:08 +0900

    画面遷移を実装（YouTube起動機能は未実装）

--- MusicAlert/src/main/java/com/example/demo/MusicAlert.java
+++ MusicAlert/src/main/java/com/example/demo/MusicAlert.java
@@ -4,13 +4,19 @@ import java.util.Date;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class MusicAlert {
 	
+	@RequestMapping("/")
+	public String start() {
+		return "first";
+	}
+	
 	@RequestMapping("/home")
-	public ModelAndView index() {
+	public ModelAndView nowTime() {
 		
 		Date now = new Date();
 		ModelAndView mav = new ModelAndView();
@@ -19,8 +25,14 @@ public class MusicAlert {
 		return mav;
 	}
 	
-	@RequestMapping("/")
-	public String start() {
-		return "home";
+	
+	@RequestMapping("/afterSetting")
+	public ModelAndView post(@RequestParam("alertTime") String time,
+				             @RequestParam("url") String link) {
+		ModelAndView mav = new ModelAndView();
+		mav.addObject("setTime", time);
+		mav.addObject("url", link);
+		return mav;
 	}
+	
 }
